23251e85db858e21a63a9d42f5415202429d70cf
Ryan-Zayne <phoenixastro97@gmail.com>
2025-06-13 19:07:30 +0100

    add pwa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1631,3 +1631,16 @@
   };
   eduJs.i();
 })(window, document, jQuery);
+
+// PWA Service Worker registration
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => {
+        console.log('Service Worker registered with scope:', reg.scope);
+      })
+      .catch(err => {
+        console.error('Service Worker registration failed:', err);
+      });
+  });
+}
