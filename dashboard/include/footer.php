7724e172595a2e04baa483064eec1fbda8fe02bc
Ryan-Zayne <phoenixastro97@gmail.com>
2025-06-16 14:21:02 +0100

    .

---
+++ dashboard/include/footer.php
@@ -0,0 +1,30 @@
+<!-- Start Footer Area -->
+<footer class="rbt-footer footer-style-1 bg-color-white overflow-hidden">
+    <div class="rbt-separator-mid">
+        <div class="container">
+            <hr class="rbt-separator m-0" />
+        </div>
+    </div>
+    <!-- Start Copyright Area -->
+    <div class="copyright-area copyright-style-1 ptb--20">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-12">
+                    <p class="rbt-link-hover text-lg-start text-center">
+                        Copyright 2025 <a href="index.html">SteezTech</a>. All Rights Reserved
+                    </p>
+                </div>
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-12">
+                    <ul
+                        class="copyright-link rbt-link-hover justify-content-center justify-content-lg-end mt_sm--10 mt_md--10">
+                        <li><a href="#">Terms of service</a></li>
+                        <li><a href="privacy-policy.html">Privacy policy</a></li>
+                        <li><a href="index.html">Back to Home</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Copyright Area -->
+</footer>
+<!-- End Footer Area -->
---
+++ dashboard/student-dashboard.php
@@ -0,0 +1,326 @@
+<?php include 'include/header.php'; ?>
+
+						<div class="rbt-dashboard-content bg-color-white rbt-shadow-box mb--60">
+							<div class="content">
+								<div class="section-title">
+									<h4 class="rbt-title-style-3">Dashboard</h4>
+								</div>
+								<div class="row g-5">
+									<!-- Start Single Card -->
+									<div class="col-lg-4 col-md-4 col-sm-6 col-12">
+										<div
+											class="rbt-counterup variation-01 rbt-hover-03 rbt-border-dashed bg-primary-opacity"
+										>
+											<div class="inner">
+												<div class="rbt-round-icon bg-primary-opacity">
+													<i class="feather-book-open"></i>
+												</div>
+												<div class="content">
+													<h3 class="counter without-icon color-primary">
+														<span class="odometer" data-count="5">00</span>
+													</h3>
+													<span class="rbt-title-style-2 d-block">Active Courses</span>
+												</div>
+											</div>
+										</div>
+									</div>
+									<!-- End Single Card -->
+
+									<!-- Start Single Card -->
+									<div class="col-lg-4 col-md-4 col-sm-6 col-12">
+										<div
+											class="rbt-counterup variation-01 rbt-hover-03 rbt-border-dashed bg-secondary-opacity"
+										>
+											<div class="inner">
+												<div class="rbt-round-icon bg-secondary-opacity">
+													<i class="feather-monitor"></i>
+												</div>
+												<div class="content">
+													<h3 class="counter without-icon color-secondary">
+														<span class="odometer" data-count="3">00</span>
+													</h3>
+													<span class="rbt-title-style-2 d-block">Assignments Due</span>
+												</div>
+											</div>
+										</div>
+									</div>
+									<!-- End Single Card -->
+
+									<!-- Start Single Card -->
+									<div class="col-lg-4 col-md-4 col-sm-6 col-12">
+										<div
+											class="rbt-counterup variation-01 rbt-hover-03 rbt-border-dashed bg-violet-opacity"
+										>
+											<div class="inner">
+												<div class="rbt-round-icon bg-violet-opacity">
+													<i class="feather-award"></i>
+												</div>
+												<div class="content">
+													<h3 class="counter without-icon color-violet">
+														<span class="odometer" data-count="78">00</span>%
+													</h3>
+													<span class="rbt-title-style-2 d-block">Overall Progress</span>
+												</div>
+											</div>
+										</div>
+									</div>
+									<!-- End Single Card -->
+								</div>
+
+								<!-- Current Courses Section -->
+								<div class="row g-5 mt--30">
+									<div class="col-12">
+										<div class="section-title">
+											<h4 class="rbt-title-style-3">Current Courses</h4>
+										</div>
+										<div class="row g-5">
+											<!-- Course 1 -->
+											<div class="col-lg-4 col-md-6 col-12">
+												<div class="rbt-card variation-01 rbt-hover">
+													<div class="rbt-card-img">
+														<a href="#">
+															<img
+																src="../assets/images/course/course-01.jpg"
+																alt="Course Image"
+															/>
+														</a>
+													</div>
+													<div class="rbt-card-body">
+														<div class="rbt-card-top">
+															<div class="rbt-review">
+																<div class="rating">
+																	<i class="fas fa-star"></i>
+																	<i class="fas fa-star"></i>
+																	<i class="fas fa-star"></i>
+																	<i class="fas fa-star"></i>
+																	<i class="fas fa-star"></i>
+																</div>
+																<span class="rating-count">(15)</span>
+															</div>
+															<div class="rbt-bookmark-btn">
+																<a class="rbt-round-btn" title="Bookmark" href="#">
+																	<i class="feather-bookmark"></i>
+																</a>
+															</div>
+														</div>
+														<h4 class="rbt-card-title">
+															<a href="#">Introduction to Programming</a>
+														</h4>
+														<ul class="rbt-meta">
+															<li><i class="feather-book"></i>12 Lessons</li>
+															<li><i class="feather-users"></i>50 Students</li>
+														</ul>
+														<div class="rbt-progress-style-1">
+															<div class="single-progress">
+																<h6 class="rbt-title-style-2">Progress</h6>
+																<div class="progress">
+																	<div
+																		class="progress-bar"
+																		role="progressbar"
+																		style="width: 65%"
+																	></div>
+																</div>
+																<span class="rbt-title-style-2">65%</span>
+															</div>
+														</div>
+														<div class="rbt-card-bottom">
+															<a class="rbt-btn btn-sm hover-icon-reverse" href="#">
+																<span class="icon-reverse-wrapper">
+																	<span class="btn-text">Continue</span>
+																	<span class="btn-icon"
+																		><i class="feather-arrow-right"></i
+																	></span>
+																	<span class="btn-icon"
+																		><i class="feather-arrow-right"></i
+																	></span>
+																</span>
+															</a>
+														</div>
+													</div>
+												</div>
+											</div>
+
+											<!-- Course 2 -->
+											<div class="col-lg-4 col-md-6 col-12">
+												<div class="rbt-card variation-01 rbt-hover">
+													<div class="rbt-card-img">
+														<a href="#">
+															<img
+																src="../assets/images/course/course-02.jpg"
+																alt="Course Image"
+															/>
+														</a>
+													</div>
+													<div class="rbt-card-body">
+														<div class="rbt-card-top">
+															<div class="rbt-review">
+																<div class="rating">
+																	<i class="fas fa-star"></i>
+																	<i class="fas fa-star"></i>
+																	<i class="fas fa-star"></i>
+																	<i class="fas fa-star"></i>
+																	<i class="fas fa-star"></i>
+																</div>
+																<span class="rating-count">(10)</span>
+															</div>
+															<div class="rbt-bookmark-btn">
+																<a class="rbt-round-btn" title="Bookmark" href="#">
+																	<i class="feather-bookmark"></i>
+																</a>
+															</div>
+														</div>
+														<h4 class="rbt-card-title">
+															<a href="#">Web Development Basics</a>
+														</h4>
+														<ul class="rbt-meta">
+															<li><i class="feather-book"></i>15 Lessons</li>
+															<li><i class="feather-users"></i>45 Students</li>
+														</ul>
+														<div class="rbt-progress-style-1">
+															<div class="single-progress">
+																<h6 class="rbt-title-style-2">Progress</h6>
+																<div class="progress">
+																	<div
+																		class="progress-bar"
+																		role="progressbar"
+																		style="width: 15%"
+																	></div>
+																</div>
+																<span class="rbt-title-style-2">15%</span>
+															</div>
+														</div>
+														<div class="rbt-card-bottom">
+															<a class="rbt-btn btn-sm hover-icon-reverse" href="#">
+																<span class="icon-reverse-wrapper">
+																	<span class="btn-text">Continue</span>
+																	<span class="btn-icon"
+																		><i class="feather-arrow-right"></i
+																	></span>
+																	<span class="btn-icon"
+																		><i class="feather-arrow-right"></i
+																	></span>
+																</span>
+															</a>
+														</div>
+													</div>
+												</div>
+											</div>
+
+											<!-- Course 3 -->
+											<div class="col-lg-4 col-md-6 col-12">
+												<div class="rbt-card variation-01 rbt-hover">
+													<div class="rbt-card-img">
+														<a href="#">
+															<img
+																src="../assets/images/course/course-03.jpg"
+																alt="Course Image"
+															/>
+														</a>
+													</div>
+													<div class="rbt-card-body">
+														<div class="rbt-card-top">
+															<div class="rbt-review">
+																<div class="rating">
+																	<i class="fas fa-star"></i>
+																	<i class="fas fa-star"></i>
+																	<i class="fas fa-star"></i>
+																	<i class="fas fa-star"></i>
+																	<i class="fas fa-star"></i>
+																</div>
+																<span class="rating-count">(20)</span>
+															</div>
+															<div class="rbt-bookmark-btn">
+																<a class="rbt-round-btn" title="Bookmark" href="#">
+																	<i class="feather-bookmark"></i>
+																</a>
+															</div>
+														</div>
+														<h4 class="rbt-card-title">
+															<a href="#">Database Fundamentals</a>
+														</h4>
+														<ul class="rbt-meta">
+															<li><i class="feather-book"></i>10 Lessons</li>
+															<li><i class="feather-users"></i>35 Students</li>
+														</ul>
+														<div class="rbt-progress-style-1">
+															<div class="single-progress">
+																<h6 class="rbt-title-style-2">Progress</h6>
+																<div class="progress">
+																	<div
+																		class="progress-bar"
+																		role="progressbar"
+																		style="width: 100%"
+																	></div>
+																</div>
+																<span class="rbt-title-style-2">100%</span>
+															</div>
+														</div>
+														<div class="rbt-card-bottom">
+															<a class="rbt-btn btn-sm hover-icon-reverse" href="#">
+																<span class="icon-reverse-wrapper">
+																	<span class="btn-text">View Certificate</span>
+																	<span class="btn-icon"
+																		><i class="feather-arrow-right"></i
+																	></span>
+																	<span class="btn-icon"
+																		><i class="feather-arrow-right"></i
+																	></span>
+																</span>
+															</a>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<?php include 'include/footer.php'; ?>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- JS
+	============================================ -->
+		<!-- Modernizer JS -->
+		<script src="../assets/js/vendor/modernizr.min.js"></script>
+		<!-- jQuery JS -->
+		<script src="../assets/js/vendor/jquery.js"></script>
+		<!-- Bootstrap JS -->
+		<script src="../assets/js/vendor/bootstrap.min.js"></script>
+		<!-- sal.js -->
+		<script src="../assets/js/vendor/sal.js"></script>
+		<!-- Dark Mode Switcher -->
+		<script src="../assets/js/vendor/js.cookie.js"></script>
+		<script src="../assets/js/vendor/jquery.style.switcher.js"></script>
+		<script src="../assets/js/vendor/swiper.js"></script>
+		<script src="../assets/js/vendor/jquery-appear.js"></script>
+		<script src="../assets/js/vendor/odometer.js"></script>
+		<script src="../assets/js/vendor/backtotop.js"></script>
+		<script src="../assets/js/vendor/isotop.js"></script>
+		<script src="../assets/js/vendor/imageloaded.js"></script>
+
+		<script src="../assets/js/vendor/wow.js"></script>
+		<script src="../assets/js/vendor/waypoint.min.js"></script>
+		<script src="../assets/js/vendor/easypie.js"></script>
+		<script src="../assets/js/vendor/text-type.js"></script>
+		<script src="../assets/js/vendor/jquery-one-page-nav.js"></script>
+		<script src="../assets/js/vendor/bootstrap-select.min.js"></script>
+		<script src="../assets/js/vendor/jquery-ui.js"></script>
+		<script src="../assets/js/vendor/magnify-popup.min.js"></script>
+		<script src="../assets/js/vendor/paralax-scroll.js"></script>
+		<script src="../assets/js/vendor/paralax.min.js"></script>
+		<script src="../assets/js/vendor/countdown.js"></script>
+		<script src="../assets/js/vendor/plyr.js"></script>
+		<script src="../assets/js/vendor/jodit.min.js"></script>
+		<script src="../assets/js/vendor/Sortable.min.js"></script>
+
+		<!-- Main JS -->
+		<script src="../assets/js/main.js"></script>
+		<script src="../assets/js/sidebar.js"></script>
+		<!-- <script src="assets/js/pwa.js"></script> -->
+	</body>
+</html>
